bb8c44ecf3cb69e0f907b80e528fb5d4bc9cd5d0
franksanchez205 <franksanchez205@gmail.com>
2026-08-26 14:11:03 -0500

    Ejercicio 1 · Método void sin parámetros

---
+++ talles/src/ejercicio1.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+    public class ejercicio1 {
+
+        public static void main(String[] args) {
+
+            mostrarBienvenida();
+
+        }
+
+        public static void mostrarBienvenida() {
+
+            JOptionPane.showMessageDialog(null, "¡Bienvenido al instituto!");
+
+        }
+    }
+
+
+
+
+
+
