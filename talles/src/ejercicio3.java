747bbec21f582e66eb4f26f6b24a06f79e273a2f
franksanchez205 <franksanchez205@gmail.com>
2026-08-26 14:32:45 -0500

    Ejercicio 3 · Método con retorno numérico

---
+++ talles/src/ejercicio3.java
@@ -0,0 +1,21 @@
+public class ejercicio3 {
+
+    public static void main(String[] args) {
+
+        double area = calcularAreaRectangulo(10, 5);
+
+        System.out.println("El área del rectángulo es: " + area);
+
+    }
+
+    public static double calcularAreaRectangulo(double base, double altura) {
+
+        double area = base * altura;
+
+        return area;
+
+    }
+
+
+
+}
